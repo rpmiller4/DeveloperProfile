89f55cbd977fe7dcca68383c27838aa8c2486912
Robert Miller <rpmiller4@gmail.com>
2021-03-25 23:55:32 -0700

    Update NavMenu and add the new QuestionsAndAnswersComponent.

--- DeveloperProfile/ClientApp/src/App.js
+++ DeveloperProfile/ClientApp/src/App.js
@@ -4,18 +4,20 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-
+import { QuestionsAndAnswers } from './components/QuestionsAndAnswers';
 import './custom.css'
+
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/questions-and-answers' component={QuestionsAndAnswers} />
       </Layout>
     );
   }
