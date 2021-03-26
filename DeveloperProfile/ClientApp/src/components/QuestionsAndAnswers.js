59c5ec038175c510afc1b859ff799b504a281eac
Robert Miller <rpmiller4@gmail.com>
2021-03-26 00:05:45 -0700

    Create Tensorflow-powered React component.

--- DeveloperProfile/ClientApp/src/components/QuestionsAndAnswers.js
+++ DeveloperProfile/ClientApp/src/components/QuestionsAndAnswers.js
@@ -1,31 +1,69 @@
 import React, { Component } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as qna from '@tensorflow-models/qna';
 
 export class QuestionsAndAnswers extends Component {
   static displayName = QuestionsAndAnswers.name;
 
   constructor(props) {
     super(props);
-    this.state = { currentCount: 0 };
-    this.incrementCounter = this.incrementCounter.bind(this);
+    this.state = { context: "", question: "", answer: "" };
+
+    this.respond = this.respond.bind(this);
+    this.setContext = this.setContext.bind(this);
+    this.setQuestion = this.setQuestion.bind(this);
   }
 
-  incrementCounter() {
+  setContext(event) {
     this.setState({
-      currentCount: this.state.currentCount + 1
+      question: event.target.value
+    });
+  }
+
+  setQuestion(event) {
+    this.setState({
+      context: event.target.value
     });
   }
 
   render() {
     return (
       <div>
-        <h1>Counter</h1>
+        <h1>Q And A (ML)</h1>
 
-        <p>This is a simple example of a React component.</p>
+        <p>This is a simple example of a the QNA Tensorflow component.</p>
 
-        <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
+        <div class="form-group">
+          <label for="questionContext">Insert Context Here</label>
+          <textarea class="form-control" id="questionContext" rows="3" onChange={this.setContext}>Enter context here</textarea>
+          <label for="answerTextArea">Answer:</label>
+          <textarea class="form-control" id="questionArea" rows="3" onChange={this.setQuestion}></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={() => this.submitQuestion()}>Get Answers</button>
 
-        <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+
+        <p>{this.state.answer}</p>
       </div>
     );
   }
+
+  async submitQuestion() {
+    await this.respond(this.state.question, this.state.context)
+  }
+
+  async respond(question, passage) {
+    console.log(question);
+    console.log(passage);
+    console.log(qna.version);
+    // Load the model.
+    await qna.load().then(model => {
+      // Find the answers
+      model.findAnswers(passage, question).then(answers => {
+        this.setState({
+          answer: JSON.stringify(answers)
+        });
+      });
+    });
+  }
+
 }
